a7991217fb71b33521f804835aae4fa3031913d6
Emerson <emersonteles21@gmail.com>
2021-07-13 01:56:41 -0300

    s

--- src/routes.js
+++ src/routes.js
@@ -4,22 +4,29 @@ const Bogosort = require('./Components/bogosort.js');
 const router = Router();
 
 router.get('/Tema/:intN',(req,res)=>{  
+
     const intN = parseInt(req.params.intN);  
     const [sortedArray, timing, attempts] = Bogosort(intN);
-
+    
     try{
-        if(intN > 1){
-            res.status(200).json(
-                {   'Lista de numeros': `${sortedArray}`,
-                    'Tempo de execução': `${timing}ms`,
-                    'Tentativas': `${attempts}`,
-                    'Tamanho do array': `${intN}`,
-                });
+        if(!isNaN(intN)){
+            if(intN > 1){
+                res.status(200).json(
+                    {   'Lista de numeros': `${sortedArray}`,
+                        'Tempo de execução': `${timing}ms`,
+                        'Tentativas': `${attempts}`,
+                        'Tamanho do array': `${intN}`,
+                    });
+            }else{
+                throw new Error ("Numero digitado igual ou menor que 1.")
+            }
         }else{
-            throw new Error ("Numero igual ou menor que 1.")
+            throw new Error ("Valor digitado não é um numero.")
         }
     }catch(err){
-        console.log(err)
+        res.status(400).json({
+            ERROR: err.message,
+        })
     };
 });
 
